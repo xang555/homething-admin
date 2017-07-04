be5fbe2008b09117ad44d7c8fba552654160531c
xang <xang.ultimate@gmail.com>
2017-07-04 17:33:10 +0700

    create navbar

--- src/Component/Navbar/index.js
+++ src/Component/Navbar/index.js
@@ -1,3 +1,36 @@
 /**
  * Created by xang on 04/07/2017.
  */
+import React , { Component } from 'react';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
+import logo from '../../Icon/banner_about.png';
+import { Link } from 'react-router-dom';
+
+
+class HomethingNavbar extends Component {
+
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+
+        return (
+            <Navbar fixedTop>
+                <Navbar.Header>
+                    <Navbar.Brand>
+                        <Link to="/"><img src={logo} alt="homething admin" className="logo-homething"/></Link>
+                        <Link to="/"><span className="band-text-color">Homething Admin</span></Link>
+                    </Navbar.Brand>
+                </Navbar.Header>
+                <Nav pullRight>
+                    <NavItem eventKey={1} href="#"><span className="glyphicon glyphicon-log-out"/>  Logout</NavItem>
+                </Nav>
+            </Navbar>
+        );
+
+    }
+
+}
+
+export default HomethingNavbar;
---
+++ src/Config.js
@@ -0,0 +1,5 @@
+/**
+ * Created by xang on 04/07/2017.
+ */
+
+export const  API_URL = "https://homethingapi.xangnam.com/";
